9aa0731b2be060b2e126ad556b440a625459256a
Hummel009 <emperorbur@gmail.com>
2022-11-29 12:44:42 +0300

    *

--- amogus.js
+++ amogus.js
@@ -1,20 +1,47 @@
 document.getElementById("lang-en").onclick = function() {
 	toEng()
 };
+
 document.getElementById("lang-be").onclick = function() {
 	toBel()
 };
 
+loader();
+
+function loader() {
+	loadIfExists("also");
+	loadIfExists("day");
+	loadIfExists("intro");
+	loadIfExists("licenseid");
+	loadIfExists("list");
+	loadIfExists("more");
+	loadIfExists("support");
+	loadIfExists("title");
+	loadIfExists("workers");
+	loadIfExists("langs");
+	loadIfExists("list2");
+	loadIfExists("searchrc");
+	loadIfExists("znaist");
+}
+
 function toEng() {
 	if (typeof (Storage) !== "undefined") {
-		localStorage.setItem("langss", "Choose a language");
-		localStorage.setItem("titles", "Main Page");
-		localStorage.setItem("lists", "List of people");
-		localStorage.setItem("intros", "Welcome, visitor (please join us) to this internet portal where you can find information about some Belarusian writers. Read and learn new things!");
-		document.getElementById("langs").innerHTML = localStorage.getItem("langss");
-		document.getElementById("title").innerHTML = localStorage.getItem("titles");
-		document.getElementById("list").innerHTML = localStorage.getItem("lists");
-		document.getElementById("intro").innerHTML = localStorage.getItem("intros");
+		localStorage.setItem("also", "See also");
+		localStorage.setItem("day", "Person of the day");
+		localStorage.setItem("intro", "Welcome, visitor (please join us) to this internet portal where you can find information about some Belarusian writers. Read and learn new things!");
+		localStorage.setItem("licenseid", "Community materials are available under the terms of the CC-BY-SA license, unless otherwise specified.");
+		localStorage.setItem("list", "List of people");
+		localStorage.setItem("more", "Read more");
+		localStorage.setItem("support", "For technical support, please contact the BSUIR FCSIS PSIT frontenders team.");
+		localStorage.setItem("title", "Main Page");
+		localStorage.setItem("workers", "Dungeon Masters");
+
+		localStorage.setItem("langs", "Choose a language");
+		localStorage.setItem("list2", "List of people");
+		localStorage.setItem("searchrc", "Search");
+		localStorage.setItem("znaist", "Find");
+
+		loader();
 	} else {
 		document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 	}
@@ -22,15 +49,31 @@ function toEng() {
 
 function toBel() {
 	if (typeof (Storage) !== "undefined") {
-		localStorage.setItem("langss", "Перакласці");
-		localStorage.setItem("titles", "Галоўная");
-		localStorage.setItem("lists", "Спіс дзеячаў");
-		localStorage.setItem("intros", "Сардэчна запрашаем, наведвальнік (калі ласка, далучайся да нас) на гэты інтэрнэт-партал, дзе вы можаце знайсці інфармацыю аб некаторых беларускіх пісьменніках. Чытайце і пазнавайце новае!");
-		document.getElementById("langs").innerHTML = localStorage.getItem("langss");
-		document.getElementById("title").innerHTML = localStorage.getItem("titles");
-		document.getElementById("list").innerHTML = localStorage.getItem("lists");
-		document.getElementById("intro").innerHTML = localStorage.getItem("intros");
+		localStorage.setItem("also", "Глядзіце таксама");
+		localStorage.setItem("day", "Дзеяч дня");
+		localStorage.setItem("intro", "Сардэчна запрашаем, наведвальнік (калі ласка, далучайся да нас) на гэты інтэрнэт-партал, дзе вы можаце знайсці інфармацыю аб некаторых беларускіх пісьменніках. Чытайце і пазнавайце новае!");
+		localStorage.setItem("licenseid", "Матэрыялы супольнасці даступныя ўадпаведнасці з умовамі ліцэнзіі CC-BY-SA, калі не пазначана іншае.");
+		localStorage.setItem("list", "Спіс дзеячаў");
+		localStorage.setItem("more", "Чытаць падрабязней");
+		localStorage.setItem("support", "Па пытаннях тэхнічнай падтрымкі звяртацца да каманды франтэндэраў БДУІР ФКСіС ПЗІТ.");
+		localStorage.setItem("title", "Галоўная");
+		localStorage.setItem("workers", "Распрацоўшчыкі");
+
+		localStorage.setItem("list2", "Спіс дзеячаў");
+		localStorage.setItem("langs", "Перакласці");
+		localStorage.setItem("searchrc", "Пошук");
+		localStorage.setItem("znaist", "Знайсці");
+
+		loader();
 	} else {
 		document.getElementById("result").innerHTML = "На жаль, ваш браўзер не працуе са сховішчам даных карыстальніка.";
+	}
+}
+
+function loadIfExists(string) {
+	if (typeof (document.getElementById(string)) != 'undefined' && document.getElementById(string) != null) {
+		if (localStorage.getItem(string) != null) {
+			document.getElementById(string).innerHTML = localStorage.getItem(string);
+		}
 	}
 }
